1f913b08758c24e3caeab058073976c3681a04d1
Vyacheslav <viacheslav87klerik@gmail.com>
2022-12-05 01:17:27 +0300

    Сделано ДЗ. Экзамен - практика. 2-й курс.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] Arr = new int[] {1, 2, 3, 4};
+        for (int i = 0; i < Arr.length; i++) {
+            System.out.println(Arr[i]);
+        }
+        for (int j = Arr.length-1; j>=0;j--) {
+            System.out.println(Arr[j]);
+        }
+
+        String str = "abc d ef g";
+        String str1 = str.replace(" ", "");
+        System.out.println(str1);
+
     }
 }
